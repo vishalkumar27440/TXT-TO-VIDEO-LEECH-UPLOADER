a660044d66a27d8c22ace416cdf45a6b73de8e62
vishalkumar27440 <vishalkumar27440@gmail.com>
2026-07-20 22:42:25 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -6,10 +6,6 @@ API_ID = int(environ.get("API_ID", "21595709"))
 API_HASH = environ.get("API_HASH", "6b683b86a90c6fae0fbe50a6494bdd53")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8919674457:AAFoqHcf-ywkOCxJJupfEenU2C0LC4hYEWU")
 
-# Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/NEET_QUIZ_HINDI_MEDIUM")  # Channel link
-
 # Admin Configuration
 ADMINS = list(map(int, environ.get("ADMINS", "8032590813").split()))
 
